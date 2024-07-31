c44adb9d484eb0347e2f546851d25e7481ebcb19
Muhammad Hassan Atiq <hassanatiq496@gmail.com>
2024-08-01 02:19:46 +0500

    db connection and app server initialized

---
+++ src/db/dbConnection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+// Connect to MongoDB
+
+const dbConnection = async() =>{
+    try {
+        const mongoConnection = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        console.log(`MongoDB connected sucessfully \n DB Host: ${mongoConnection.connection.host}`);
+    } catch (error) {
+        console.error(`Error in DB connection: ${error}`);
+        process.exit(1)
+    }
+}
+
+
+export default dbConnection;
