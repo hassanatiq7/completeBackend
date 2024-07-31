b153b48550209e274ae555bd538d221986efacb7
Muhammad Hassan Atiq <hassanatiq496@gmail.com>
2024-08-01 02:35:41 +0500

    Database connected

--- src/server.js
+++ src/server.js
@@ -1,3 +1,12 @@
 import dotenv from "dotenv";
+import express from "express";
+import dbConnection from "./db/dbConnection.js";
 
-dotenv.config();
+dotenv.config({path: "./env"});
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+
+dbConnection()
